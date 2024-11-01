4355a78a2c3a9954bea3c6041d2ec73fd5892706
that30sCoder <rahulksingh1092@gmail.com>
2024-11-01 17:26:09 +0530

    Accounts microservice

---
+++ src/main/java/com/that30scoder/accounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.that30scoder.accounts.repository;
+
+import com.that30scoder.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
